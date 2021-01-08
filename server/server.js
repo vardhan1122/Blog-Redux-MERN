ddfc2eeba1774d843f4aabd09e9afbfb6af6259c
Govardhan Reddy <vardhan.js90@gmail.com>
2021-01-08 11:06:32 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -26,22 +26,22 @@ const port = process.env.PORT || 5000;
 // const port = process.env.PORT;
 
 // for React Application Static Home Page
-app.get('/', (request , response) => {
-    response.send(`<h2>Welcome to BigBasket Express Server</h2>`);
-});
-
-// for React Application Deployment Home Page
-// app.use(express.static(path.join(__dirname , 'client' , 'build')));
-// app.get('/', (request,response) => {
-//     response.sendFile(path.join(__dirname , 'client' , 'build' , 'index.html'));
+// app.get('/', (request , response) => {
+//     response.send(`<h2>Welcome to BigBasket Express Server</h2>`);
 // });
 
-// if(process.env.NODE_ENV === "production"){
-//     app.use(express.static(path.join(__dirname , 'client' , 'build')));
-//     app.get('/', (request,response) => {
-//         response.sendFile(path.join(__dirname , 'client' , 'build' , 'index.html'));
-//     });
-// }
+// for React Application Deployment Home Page
+app.use(express.static(path.join(__dirname , 'client' , 'build')));
+app.get('/', (request,response) => {
+    response.sendFile(path.join(__dirname , 'client' , 'build' , 'index.html'));
+});
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname , 'client' , 'build')));
+    app.get('/', (request,response) => {
+        response.sendFile(path.join(__dirname , 'client' , 'build' , 'index.html'));
+    });
+}
 
 // connect to Mongo DB Database
 mongoose.connect(process.env.MONGO_DB_CLOUD_URL, {
